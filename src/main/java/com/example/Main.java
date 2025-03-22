dbe4cf331b1814cd5525c39b870029a9016bef54
Gia Chí Đặng <danggiachi@gmail.com>
2025-03-22 20:09:55 +0100

    Complete

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,34 +3,51 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetRecord greet(){
-        GreetRecord response = new GreetRecord(
-            "Hello",
-            List.of("Python", "Java", "C++"),
-            new Person("Angela", 20, 160)
-            );
-        return response;
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
-    record Person(
-        String name,
-        int age,
-        double height
-        ){}
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
 
-    record GreetRecord(
-        String greet, 
-        List<String> favProgrammingLanguages,
-        Person person
-        ){}
+    record NewCustomerRequest(String name, String email, Integer age){}
+
+    @PostMapping
+    public void addCustomers(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request){
+        Customer customer = customerRepository.findById(id).get();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
 }
